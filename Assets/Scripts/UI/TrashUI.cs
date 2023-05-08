0377ece889b971b831cf2ef82ca98e4a0cd38973
mintchoko <39862320+mintchoko@users.noreply.github.com>
2023-05-08 20:28:28 +0900

    battleUI 5/8

--- Assets/Scripts/UI/TrashUI.cs
+++ Assets/Scripts/UI/TrashUI.cs
@@ -5,14 +5,8 @@ using UnityEngine;
 public class TrashUI : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void TrashButtonClick()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
