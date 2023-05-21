cbd2bec5271549c6573b7d646b1067836046382e
komorie <51258965+komorie@users.noreply.github.com>
2023-05-22 02:50:36 +0900

    JSON Path Bug fix
    빌드 시 JSON 경로를 찾을 수 없는 문제 해결.

--- Assets/Scripts/Util/AssetLoader.cs
+++ Assets/Scripts/Util/AssetLoader.cs
@@ -27,30 +27,6 @@ public class AssetLoader : Singleton<AssetLoader>
         return Resources.LoadAll<T>(path);
     }
 
-    //특정 폴더의 하위 폴더의 에셋들까지 전부 배열로 가져오는 함수
-    public T[] LoadAllInSubfolders<T>(string path) where T : Object
-    {
-        List<T> assets = new List<T>();
-
-        // "Assets"로 시작하는 경로로 변경합니다.
-        string fullPath = $"Assets/Resources/{path}";
-
-        if (Directory.Exists(fullPath))
-        {
-            string[] subfolders = Directory.GetDirectories(fullPath, "*", SearchOption.AllDirectories);
-
-            foreach (string subfolder in subfolders)
-            {
-                // 경로를 리소스 경로 형식으로 변환합니다.
-                string relativePath = $"{fullPath}/{Path.GetFileName(subfolder)}".Substring("Assets/Resources/".Length);
-                T[] subfolderAssets = LoadAll<T>(relativePath);
-                assets.AddRange(subfolderAssets);
-            }
-        }
-
-        return assets.ToArray();
-    }
-
 
 
     //프리팹을 (안불러왔으면 불러오고) 복제해서 소환
